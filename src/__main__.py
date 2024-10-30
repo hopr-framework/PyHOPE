a550e2fa60d137fdf89a6590fcb678395e8357e2
Marcel Blind <marcel.blind@iag.uni-stuttgart.de>
2024-10-30 10:45:09 +0100

    rename in all new files and small consistency fixes

--- src/__main__.py
+++ src/__main__.py
@@ -3,24 +3,24 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# This file is part of UVWXYZ
+# This file is part of PyHOPE
 #
 # Copyright (c) 2022-2024 Andrea Beck
 #
-# UVWXYZ is free software: you can redistribute it and/or modify it under the
+# PyHOPE is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# UVWXYZ is distributed in the hope that it will be useful, but WITHOUT ANY
+# PyHOPE is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with
-# UVWXYZ. If not, see <http://www.gnu.org/licenses/>.
+# PyHOPE. If not, see <http://www.gnu.org/licenses/>.
 
 
-from src.uvwxyz import main
+from src.pyhope import main
 
 
 if __name__ == '__main__':
