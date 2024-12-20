f046a63df7120186fc508f7b05fe5d9de801cf4f
Patrick Kopper <kopper@iag.uni-stuttgart.de>
2024-12-20 09:41:47 +0100

    Add possibility to merge HOPR files with different NGeo

--- pyhope/mesh/reader/reader_hopr.py
+++ pyhope/mesh/reader/reader_hopr.py
@@ -32,6 +32,7 @@ import sys
 # import tempfile
 # import time
 # import traceback
+from string import digits
 from typing import cast
 # ----------------------------------------------------------------------------------------------------------------------------------
 # Third-party libraries
@@ -51,10 +52,31 @@ import numpy as np
 # ==================================================================================================================================
 
 
+def NDOFperElemType(elemType: str, nGeo: int) -> int:
+    """ Calculate the number of degrees of freedom for a given element type
+    """
+    match elemType:
+        case 'triangle':
+            return round((nGeo+1)*(nGeo+2)/2.)
+        case 'quad':
+            return round((nGeo+1)**2)
+        case 'tetra':
+            return round((nGeo+1)*(nGeo+2)*(nGeo+3)/6.)
+        case 'pyramid':
+            return round((nGeo+1)*(nGeo+2)*(2*nGeo+3)/6.)
+        case 'prism':
+            return round((nGeo+1)**(nGeo-1)*(nGeo+2)/2.)
+        case 'hexahedron':
+            return round((nGeo+1)**3)
+        case _:
+            raise ValueError(f'Unknown element type {elemType}')
+
+
 def ReadHOPR(fnames: list, mesh: meshio.Mesh) -> meshio.Mesh:
     # Local imports ----------------------------------------
     import pyhope.output.output as hopout
     import pyhope.mesh.mesh_vars as mesh_vars
+    from pyhope.basis.basis_basis import barycentric_weights, calc_vandermonde, change_basis_3D
     from pyhope.mesh.mesh_common import LINTEN
     from pyhope.mesh.mesh_common import faces, face_to_cgns
     from pyhope.mesh.mesh_vars import ELEMTYPE
@@ -69,8 +91,10 @@ def ReadHOPR(fnames: list, mesh: meshio.Mesh) -> meshio.Mesh:
 
     nodeCoords   = mesh.points
     offsetnNodes = 0
-    # offsetnSides = 0
     nSides       = 0
+
+    # Vandermonde for changeBasis
+    VdmEqHdf5ToEqMesh = np.array([])
 
     for fname in fnames:
         # Check if the file is using HDF5 format internally
@@ -89,18 +113,37 @@ def ReadHOPR(fnames: list, mesh: meshio.Mesh) -> meshio.Mesh:
 
             # Read the nodeCoords
             nodeCoords = np.array(f['NodeCoords'])
-            indices    = np.unique(nodeInfo, return_index=True)[1]
-            nodeCoords = nodeCoords[indices]
-
-            points     = np.append(points, nodeCoords, axis=0)
 
             # Read nGeo
-            nGeo       = cast(int, f.attrs['Ngeo'])
-            if nGeo != mesh_vars.nGeo:
-                # TODO: FIX THIS WITH A CHANGEBASIS
+            nGeo       = int(cast(int, f.attrs['Ngeo']))
+
+            if nGeo == mesh_vars.nGeo:
+                # only retain the unique nodes
+                indices    = np.unique(nodeInfo, return_index=True)[1]
+                nodeCoords = nodeCoords[indices]
+                points     = np.append(points, nodeCoords, axis=0)
+            else:
+                # ChangeBasis on the non-unique nodes
+                # > Currently only supported for hexahedrons
                 filename = os.path.basename(fname)
-                hopout.warning('File [󰇘]/{} has different polynomial order than the current mesh, exiting...'.format(filename))
-                sys.exit(1)
+                print(hopout.warn(f'File [󰇘]/{filename} has different polynomial order than the current mesh, converting...',
+                      length=999))
+                print(hopout.warn(f'> NGeo [HDF5] = {nGeo}, NGeo [Mesh] = {mesh_vars.nGeo}') + '\n')
+
+                # Compute the equidistant point set used by HOPR
+                xEqHdf5 = np.zeros(nGeo+1)
+                for i in range(nGeo+1):
+                    xEqHdf5[i] = 2.*float(i)/float(nGeo) - 1.
+                wBaryEqHdf5 = barycentric_weights(nGeo, xEqHdf5)
+
+                # Compute the equidistant point set used by meshIO
+                xEqMesh = np.zeros(mesh_vars.nGeo+1)
+                for i in range(mesh_vars.nGeo+1):
+                    xEqMesh[i] = 2.*float(i)/float(mesh_vars.nGeo) - 1.
+                # wBaryEqMesh = barycentric_weights(mesh_vars.nGeo, xEqMesh)
+
+                # Compute the Vandermonde matrix
+                VdmEqHdf5ToEqMesh = calc_vandermonde(nGeo+1, mesh_vars.nGeo+1, wBaryEqHdf5, xEqHdf5, xEqMesh)
 
             # Read the elemInfo and sideInfo
             elemInfo   = np.array(f['ElemInfo'])
@@ -109,24 +152,51 @@ def ReadHOPR(fnames: list, mesh: meshio.Mesh) -> meshio.Mesh:
 
             # Construct the elements, meshio format
             for elem in elemInfo:
-                # Obtain the element type
-                elemType = ELEMTYPE.inam[elem[0]]
-                if len(elemType) > 1:
-                    elemType = elemType[nGeo-2]
-                else:
-                    elemType = elemType[0]
+                # Correct ElemType if NGeo is changed
+                elemNum  = elem[0] % 100
+                elemNum += 200 if mesh_vars.nGeo > 1 else 100
 
-                linMap    = LINTEN(elem[0], order=nGeo)
-                elemNodes = np.arange(elem[4], elem[5])
-                elemNodes = np.expand_dims(nodeInfo[elemNodes[linMap]]-1+offsetnNodes, axis=0)
+                # Obtain the element type
+                elemType = ELEMTYPE.inam[elemNum]
+                if len(elemType) > 1:
+                    elemType  = elemType[0].rstrip(digits)
+                    elemType += str(NDOFperElemType(elemType, mesh_vars.nGeo))
+                else:
+                    elemType  = elemType[0]
+
+                # ChangeBasis currently only supported for hexahedrons
+                linMap    = LINTEN(elemNum, order=mesh_vars.nGeo)
+                mapLin    = {k: v for v, k in enumerate(linMap)}
+
+                if nGeo == mesh_vars.nGeo:
+                    elemIDs   = np.arange(elem[4], elem[5])
+                    elemNodes = np.zeros(len(elemIDs), dtype=np.uint64)
+                    elemNodes = elemIDs[[mapLin[np.int64(i)] for i in range(len(elemIDs))]]
+                    elemNodes = np.expand_dims(nodeInfo[elemNodes]-1+offsetnNodes, axis=0)
+                else:
+                    elemIDs   = np.arange(points.shape[0], points.shape[0]+(mesh_vars.nGeo+1)**3., dtype=np.uint64)
+                    elemNodes = np.zeros(len(elemIDs), dtype=np.uint64)
+                    elemNodes = elemIDs[[mapLin[np.int64(i)] for i in range(len(elemIDs))]]
+                    # This needs no offset as we already accounted for the number of points in elemIDs
+                    elemNodes = np.expand_dims(         elemNodes                , axis=0)
+
+                    # This is still in tensor-product format
+                    meshNodes = nodeCoords[np.arange(elem[4], elem[5])].reshape((nGeo+1, nGeo+1, nGeo+1, 3))
+                    meshNodes = meshNodes.transpose(3, 0, 1, 2)
+                    try:
+                        meshNodes = change_basis_3D(VdmEqHdf5ToEqMesh, meshNodes)
+                        meshNodes = meshNodes.transpose(1, 2, 3, 0)
+                        meshNodes = meshNodes.reshape((int((mesh_vars.nGeo+1)**3.), 3))
+                        points    = np.append(points, meshNodes, axis=0)
+                    except UnboundLocalError:
+                        raise UnboundLocalError('Something went wrong with the change basis')
 
                 try:
-                    cells[elemType] = np.append(cells[elemType], elemNodes, axis=0)
+                    cells[elemType] = np.append(cells[elemType], elemNodes.astype(np.uint64), axis=0)
                 except KeyError:
-                    cells[elemType] = elemNodes
+                    cells[elemType] = elemNodes.astype(np.uint64)
 
                 # Attach the boundary sides
-                # for index, face in enumerate(faces(elemType)):
                 sCounter = 0
                 for index in range(elem[2], elem[3]):
                     # Account for mortar sides
@@ -143,13 +213,14 @@ def ReadHOPR(fnames: list, mesh: meshio.Mesh) -> meshio.Mesh:
                     # Get the number of corners
                     nCorners  = abs(sideType % 10)
                     sideName  = 'quad' if nCorners == 4 else 'tri'
-
+                    if mesh_vars.nGeo > 1:
+                        sideName += str(NDOFperElemType(sideName, mesh_vars.nGeo))
                     sideNodes = np.expand_dims(corners, axis=0)
 
                     try:
-                        cells[sideName] = np.append(cells[sideName], sideNodes, axis=0)
+                        cells[sideName] = np.append(cells[sideName], sideNodes.astype(np.uint64), axis=0)
                     except KeyError:
-                        cells[sideName] = sideNodes
+                        cells[sideName] = sideNodes.astype(np.uint64)
 
                     # Increment the side counter
                     sCounter += 1
@@ -167,7 +238,7 @@ def ReadHOPR(fnames: list, mesh: meshio.Mesh) -> meshio.Mesh:
                         cellsets[BCName]    = [None, np.array([nSides-1], dtype=np.uint64)]  # type: ignore
 
             # Update the offset for the next file
-            offsetnNodes += nodeCoords.shape[0]
+            offsetnNodes = points.shape[0]
 
     mesh   = meshio.Mesh(points    = points,    # noqa: E251
                          cells     = cells,     # noqa: E251
