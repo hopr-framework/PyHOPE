cbdfbeb7cae3a53ec08da8a649e7de61cc810c60
Patrick Kopper <kopper@iag.uni-stuttgart.de>
2026-03-13 15:11:33 +0100

    Linting: Fix unnecessary-literal-set (C405)

--- pyhope/mesh/reader/reader_gmsh.py
+++ pyhope/mesh/reader/reader_gmsh.py
@@ -247,7 +247,7 @@ def ReadGMSH(fnames: list) -> meshio.Mesh:
         for elem in gmshIssue:
             print(hopout.warn(f'Wrong Gmsh order {elem[1]} for element {elem[0].replace(" ", "")}'))
         elemOrders = set([int(elem[1]) for elem in gmshIssue])
-        hopout.error(f'Gmsh element order(s) {elemOrders} does not match requested mesh order {set([mesh_vars.nGeo])}')
+        hopout.error(f'Gmsh element order(s) {elemOrders} does not match requested mesh order { {mesh_vars.nGeo} }')
 
     # Convert Gmsh object to meshio object
     mesh = gmsh_to_meshio(gmsh)
